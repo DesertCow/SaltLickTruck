012e3ee95c102c5c9b6fcb1553c5d0bd732d3837
Clayton S <desertcow07@gmail.com>
2022-10-02 13:21:28 -0700

    Enabled Buttons but dead links and Name has bad chars in it

--- client/src/pages/Main_Menu.js
+++ client/src/pages/Main_Menu.js
@@ -2,6 +2,7 @@
 import { useQuery } from '@apollo/client';
 import { MainMenu_Q } from '../utils/queries';
 import React, { useState } from "react";
+import { Button } from 'react-bootstrap';
 
 function Main_Menu() {
 
@@ -18,7 +19,7 @@ function Main_Menu() {
   //   headers: { 'Content-Type': 'application/json' },
   // });
 
-  const { login, data } = useQuery(MainMenu_Q)
+  var { login, data } = useQuery(MainMenu_Q)
 
   // console.log("\n\nRESPONDSE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
   // console.log(response)
@@ -26,7 +27,7 @@ function Main_Menu() {
   console.log("\n\n=========== Login ===========")
   console.log(login)
   console.log("=========== DATA ===========")
-  const finalData = String(JSON.stringify(data))
+  var finalData = String(JSON.stringify(data))
   var dataArray = finalData.split(":")
   dataArray = String(dataArray[1])
   var dataArray2 = dataArray.split(',')
@@ -39,18 +40,18 @@ function Main_Menu() {
 
   //* Cleanup Data
   // TODO: Must be a better way to pass data via GraphQL
-  
-  dataArray2[0] = String(dataArray2[0].substring(2, dataArray2[0].length - 1))
-  dataArray2[1] = String(dataArray2[1].substring(1, dataArray2[1].length - 1))
-  dataArray2[2] = String(dataArray2[2].substring(1, dataArray2[2].length - 1))
-  dataArray2[3] = String(dataArray2[3].substring(1, dataArray2[3].length - 1))
-  dataArray2[4] = String(dataArray2[4].substring(1, dataArray2[4].length - 1))
-  dataArray2[5] = String(dataArray2[5].substring(1, dataArray2[5].length - 1))
-  dataArray2[6] = String(dataArray2[6].substring(1, dataArray2[6].length - 1))
-  dataArray2[7] = String(dataArray2[7].substring(1, dataArray2[7].length - 1))
-  dataArray2[8] = String(dataArray2[8].substring(1, dataArray2[8].length - 3))
-  // dataArray2[0] = String(dataArray2[0].split('"'))
-  // dataArray2[0] = String(temp)
+  var finalArray = []
+  finalArray[0] = dataArray2[0].substring(2, dataArray2[0].length - 1)
+  // finalArray[1] = String(dataArray2[1].substring(1, dataArray2[1].length - 1))
+  // finalArray[2] = String(dataArray2[2].substring(1, dataArray2[2].length - 1))
+  // finalArray[3] = String(dataArray2[3].substring(1, dataArray2[3].length - 1))
+  // finalArray[4] = String(dataArray2[4].substring(1, dataArray2[4].length - 1))
+  // finalArray[5] = String(dataArray2[5].substring(1, dataArray2[5].length - 1))
+  // finalArray[6] = String(dataArray2[6].substring(1, dataArray2[6].length - 1))
+  // finalArray[7] = String(dataArray2[7].substring(1, dataArray2[7].length - 1))
+  // finalArray[8] = String(dataArray2[8].substring(1, dataArray2[8].length - 3))
+  // finalArray[0] = String(dataArray2[0].split('"'))
+  // finalArray[0] = String(temp)
 
   // dataArray2[0] = dataArray2[0].split('"')
 
@@ -63,18 +64,18 @@ function Main_Menu() {
     <div className="d-flex flex-column min-vh-100">
 
       <h1 className="homeTitle text-center pt-4"> Salt Lick BBQ</h1>
-      <h1 className="text-center pt-4"> Main Menu!</h1>
-      <ul>
-        <li>{dataArray2[0]}</li>
-        <li>{dataArray2[1]}</li>
-        <li>{dataArray2[2]}</li>
-        <li>{dataArray2[3]}</li>
-        <li>{dataArray2[4]}</li>
-        <li>{dataArray2[5]}</li>
-        <li>{dataArray2[6]}</li>
-        <li>{dataArray2[7]}</li>
-        {/* <li>{dataArray2[8].split('[')}</li> */}
-        <li>{dataArray2[8]}</li>
+      <h1 className="text-center pt-4 menuTitle"> Main Menu</h1>
+      <hr className="mt-2 mb-3" />
+      <ul className="text-center m-4">
+        <li className="mainMenuBtns m-4"><Button variant="light">{finalArray[0]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[1]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[2]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[3]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[4]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[5]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[6]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[7]}</Button>{' '}</li>
+        <li className="mainMenuBtns m-4"><Button variant="light">{dataArray2[8]}</Button>{' '}</li>
       </ul>
 
 
