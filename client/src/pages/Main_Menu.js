d97a96149d79d55dd9372c5faf2f33acc2ad6231
Clayton S <desertcow07@gmail.com>
2022-10-02 12:25:49 -0700

    Cleaned up GraphQL response to allow display

--- client/src/pages/Main_Menu.js
+++ client/src/pages/Main_Menu.js
@@ -35,7 +35,22 @@ function Main_Menu() {
   // var dataArray2 = dataArray.split("}")
   // console.log(JSON.parse(dataArray[0]))
   console.log("=========== Final ===========")
-  console.log(dataArray2[1])
+  // console.log(dataArray2[0].substring(2))
+
+  //* Cleanup Data
+  // TODO: Must be a better way to pass data via GraphQL
+  
+  dataArray2[0] = String(dataArray2[0].substring(2, dataArray2[0].length - 1))
+  dataArray2[1] = String(dataArray2[1].substring(1, dataArray2[1].length - 1))
+  dataArray2[2] = String(dataArray2[2].substring(1, dataArray2[2].length - 1))
+  dataArray2[3] = String(dataArray2[3].substring(1, dataArray2[3].length - 1))
+  dataArray2[4] = String(dataArray2[4].substring(1, dataArray2[4].length - 1))
+  dataArray2[5] = String(dataArray2[5].substring(1, dataArray2[5].length - 1))
+  dataArray2[6] = String(dataArray2[6].substring(1, dataArray2[6].length - 1))
+  dataArray2[7] = String(dataArray2[7].substring(1, dataArray2[7].length - 1))
+  dataArray2[8] = String(dataArray2[8].substring(1, dataArray2[8].length - 3))
+  // dataArray2[0] = String(dataArray2[0].split('"'))
+  // dataArray2[0] = String(temp)
 
   // dataArray2[0] = dataArray2[0].split('"')
 
@@ -50,16 +65,16 @@ function Main_Menu() {
       <h1 className="homeTitle text-center pt-4"> Salt Lick BBQ</h1>
       <h1 className="text-center pt-4"> Main Menu!</h1>
       <ul>
-        <li>{dataArray2[0].split('"')}</li>
+        <li>{dataArray2[0]}</li>
         <li>{dataArray2[1]}</li>
         <li>{dataArray2[2]}</li>
         <li>{dataArray2[3]}</li>
         <li>{dataArray2[4]}</li>
         <li>{dataArray2[5]}</li>
         <li>{dataArray2[6]}</li>
+        <li>{dataArray2[7]}</li>
+        {/* <li>{dataArray2[8].split('[')}</li> */}
         <li>{dataArray2[8]}</li>
-        <li>{dataArray2[9]}</li>
-        <li>{dataArray2[10]}</li>
       </ul>
 
 
