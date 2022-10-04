cfdfcb5a6afca822dfce5b48ab274bd703a5284e
Clayton S <desertcow07@gmail.com>
2022-10-04 14:30:59 -0700

    Broke out Footer to components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,30 @@
+
+const MainFooter = () => {
+
+
+  return (
+
+    <div className="mt-auto mb-0">
+
+      <div className="d-flex align-items-center justify-content-center">
+        <div className="d-flex flex-column">
+          <ul className="pb-1 text-center">
+            <li>
+              <p>Made by 🌵 Desert-Cow 🐄</p>
+            </li>
+            <li>
+              <p>© 2022 Monkey See Monkey Do LLC.</p>
+            </li>
+          </ul>
+        </div>
+        <div className="d-flex flex-column">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainFooter;
+
+
+//!========================= EOF =========================
