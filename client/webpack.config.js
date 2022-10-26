2e2f444f67d11015df0ed40a9d60e0bafd69c427
Clayton S <desertcow07@gmail.com>
2022-10-26 08:29:51 -0700

    Deployment Debug2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'App.js'),
+  // entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    // path: path.resolve(__dirname, 'dist'),
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ],
   },
 };
