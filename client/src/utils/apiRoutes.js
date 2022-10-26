324438a9050979ab1f2a4c8ed7fa69817c73673d
Clayton S <desertcow07@gmail.com>
2022-10-26 07:03:02 -0700

    Patch

--- client/src/utils/apiRoutes.js
+++ client/src/utils/apiRoutes.js
@@ -1,12 +1,12 @@
 
 // export const host = "http://localhost:3001";
-export const host = "http://192.168.25.22:3001";
+// export const host = "http://192.168.25.22:3001";
 // export const host = "https://saltlicktruck.herokuapp.com";
-export const loginRoute = `${host}/api/auth/login`;
-export const registerRoute = `${host}/api/auth/register`;
-export const logoutRoute = `${host}/api/auth/logout`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const mainMenuRoute = `${host}/api/menu/mainMenu`;
+// export const loginRoute = `${host}/api/auth/login`;
+// export const registerRoute = `${host}/api/auth/register`;
+// export const logoutRoute = `${host}/api/auth/logout`;
+// export const allUsersRoute = `${host}/api/auth/allusers`;
+// export const mainMenuRoute = `${host}/api/menu/mainMenu`;
 
 
 // export const sendMessageRoute = `${host}/api/messages/addmsg`;
