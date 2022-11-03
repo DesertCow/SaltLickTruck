b3698531dc0b8a8e4c7995613bcdadebab2b4966
Clayton S <desertcow07@gmail.com>
2022-11-03 15:03:27 -0700

    Orders Box now changes depending on status of order

--- client/src/components/ListOfOrders/index.js
+++ client/src/components/ListOfOrders/index.js
@@ -9,13 +9,14 @@ import { useQuery, useMutation } from '@apollo/client';
 import LoadingSplash from '../../components/LoadingSplash';
 
 
-
+import React, { useState } from 'react';
 
 // const MainMenu = ({ finalArray }) => {
 const ListOfOrders = () => {
 
   var orderList = []
   const [orderUpdate, { orderData }] = useMutation(ORDER_UPDATE_Q);
+  const [orderColor, setOrderColor] = useState("#fc7ace");
 
   function newOrderRow(item, index) {
 
@@ -28,10 +29,45 @@ const ListOfOrders = () => {
 
     var finalItemsArray = []
 
-    // console.log("==== Order Array ====")
-    // console.log(orderArray)
+    console.log("==== Index ====")
+    console.log(item)
 
     itemArray.forEach(finalItemGroom);
+
+    console.log("==== orderArray ====")
+    console.log(orderArray[3])
+
+    let initalColor
+
+    switch (orderArray[3]) {
+      case "Submitted":
+        // code block
+        initalColor = "#31a531"
+        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
+        // c[index].style.backgroundColor = blockColor;
+        // setOrderColor(blockColor)
+        break;
+      case "WIP":
+        initalColor = "#fff200"
+        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
+        // c[index].style.backgroundColor = blockColor;
+        // setOrderColor(blockColor)
+        break;
+      case "Ready":
+        initalColor = "#00ff00"
+        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
+        // c[index].style.backgroundColor = blockColor;
+        // setOrderColor(blockColor)
+        break;
+      case "Picked Up":
+        initalColor = "#a1a1a1"
+        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
+        // c[index].style.backgroundColor = blockColor;
+        // setOrderColor(blockColor)
+        break;
+      default:
+      // code block
+    }
 
     function finalItemGroom(item, index) {
 
@@ -49,9 +85,46 @@ const ListOfOrders = () => {
 
       console.log("Update Order Status!")
       console.log(event)
+      console.log(" ==== Target ==== ")
+      console.log(event.target.style.backgroundColor)
+      console.log(event)
       console.log(index)
       console.log(status)
       console.log(orderNumber)
+
+      let blockColor = "#bec1be"
+
+      var c = document.getElementById("listOfOrdersID").childNodes;
+
+      switch (status) {
+        case "Submitted":
+          // code block
+          blockColor = "#31a531"
+          console.log("Submitted = " + status + " | Color = " + blockColor)
+          c[index].style.backgroundColor = blockColor;
+          // setOrderColor(blockColor)
+          break;
+        case "WIP":
+          blockColor = "#fff200"
+          console.log("Submitted = " + status + " | Color = " + blockColor)
+          c[index].style.backgroundColor = blockColor;
+          // setOrderColor(blockColor)
+          break;
+        case "Ready":
+          blockColor = "#00ff00"
+          console.log("Submitted = " + status + " | Color = " + blockColor)
+          c[index].style.backgroundColor = blockColor;
+          // setOrderColor(blockColor)
+          break;
+        case "Picked Up":
+          blockColor = "#a1a1a1"
+          console.log("Submitted = " + status + " | Color = " + blockColor)
+          c[index].style.backgroundColor = blockColor;
+          // setOrderColor(blockColor)
+          break;
+        default:
+        // code block
+      }
 
       const { data } = await orderUpdate({
         variables: { orderNumber: orderNumber, newOrderStatus: status },
@@ -65,7 +138,10 @@ const ListOfOrders = () => {
     // console.log(finalItemsArray)
 
     orderList.push(
-      <li key={item} className="orderRow p-3 mx-2 mb-4">
+      <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: initalColor }}>
+        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: orderColor }}> */}
+        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: this.target.style.backgroundColor }}> */}
+        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4"> */}
         <div className="orderStatusBox p-2">
           <h1 className="mt-0">Order Number: {orderArray[0]}</h1>
         </div>
@@ -102,12 +178,12 @@ const ListOfOrders = () => {
             </button>
             <ul className="dropdown-menu p-2 dropMenuKitchen">
               <li><p className="dropdown-item submittedDrop text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "Submitted", String(orderArray[0]))}>Submitted</p></li>
-            <li><p className="dropdown-item submittedWIP text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "WIP", String(orderArray[0]))}>WIP</p></li>
-            <li><p className="dropdown-item submittedReady text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "Ready", String(orderArray[0]))}>Ready</p></li>
-            <li><p className="dropdown-item submittedPickedUp text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "Picked Up", String(orderArray[0]))}>Picked Up</p></li >
-          </ul >
+              <li><p className="dropdown-item submittedWIP text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "WIP", String(orderArray[0]))}>WIP</p></li>
+              <li><p className="dropdown-item submittedReady text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "Ready", String(orderArray[0]))}>Ready</p></li>
+              <li><p className="dropdown-item submittedPickedUp text-center mt-2" onClick={(event) => updateOrderStatus(index, event, "Picked Up", String(orderArray[0]))}>Picked Up</p></li >
+            </ul >
+          </div >
         </div >
-      </div >
 
       </li >)
 
@@ -123,54 +199,55 @@ const ListOfOrders = () => {
   }
 
 
-//TODO: Get Each Order From Database
-var { loading, data } = useQuery(OrderList_Q)
+  //TODO: Get Each Order From Database
+  var { loading, data } = useQuery(OrderList_Q)
 
 
 
-if (!loading) {
+  if (!loading) {
 
-  // console.log("GET ALL ORDERS!")
-  // console.log(data.getAllOrders)
+    // console.log("GET ALL ORDERS!")
+    // console.log(data.getAllOrders)
 
-  // console.log(data.getAllOrders[1])
+    console.log("GEt All Orders!")
+    console.log(data.getAllOrders[1])
 
-  data.getAllOrders.forEach(newOrderRow);
+    data.getAllOrders.forEach(newOrderRow);
 
-}
+  }
 
-// console.log(data[0])
-
-
-//TODO: Create a row for each order and fill with order data
-// data.forEach(newOrderRow);
+  // console.log(data[0])
 
 
-//TODO: Add DropDown/Button to update order status
+  //TODO: Create a row for each order and fill with order data
+  // data.forEach(newOrderRow);
 
-if (loading) {
+
+  //TODO: Add DropDown/Button to update order status
+
+  if (loading) {
+
+    return (
+      <div>
+        <LoadingSplash />
+
+        <footer className="mt-5">
+          {/* <MainFooter /> */}
+          {/* <NavFooter /> */}
+        </footer>
+      </div>
+
+    )
+
+  }
+
 
   return (
-    <div>
-      <LoadingSplash />
-
-      <footer className="mt-5">
-        {/* <MainFooter /> */}
-        {/* <NavFooter /> */}
-      </footer>
-    </div>
-
+    <ul className='mx-2 p-2' id="listOfOrdersID">
+      {/* <h1>test List of orders</h1> */}
+      {orderList}
+    </ul>
   )
-
-}
-
-
-return (
-  <ul className='mx-2 p-2'>
-    {/* <h1>test List of orders</h1> */}
-    {orderList}
-  </ul>
-)
 };
 
 export default ListOfOrders;
