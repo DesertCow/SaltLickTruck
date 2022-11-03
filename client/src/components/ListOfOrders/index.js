67e1649f3cdc6225ae837477e638b74d4df3f306
Clayton S <desertcow07@gmail.com>
2022-11-03 15:09:12 -0700

    Code Cleanup

--- client/src/components/ListOfOrders/index.js
+++ client/src/components/ListOfOrders/index.js
@@ -29,41 +29,28 @@ const ListOfOrders = () => {
 
     var finalItemsArray = []
 
-    console.log("==== Index ====")
-    console.log(item)
+    // console.log("==== Index ====")
+    // console.log(item)
 
     itemArray.forEach(finalItemGroom);
 
-    console.log("==== orderArray ====")
-    console.log(orderArray[3])
+    // console.log("==== orderArray ====")
+    // console.log(orderArray[3])
 
     let initalColor
 
     switch (orderArray[3]) {
       case "Submitted":
-        // code block
         initalColor = "#31a531"
-        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
-        // c[index].style.backgroundColor = blockColor;
-        // setOrderColor(blockColor)
         break;
       case "WIP":
         initalColor = "#fff200"
-        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
-        // c[index].style.backgroundColor = blockColor;
-        // setOrderColor(blockColor)
         break;
       case "Ready":
         initalColor = "#00ff00"
-        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
-        // c[index].style.backgroundColor = blockColor;
-        // setOrderColor(blockColor)
         break;
       case "Picked Up":
         initalColor = "#a1a1a1"
-        console.log("########## Submitted = " + orderArray[3] + " | Color = " + initalColor)
-        // c[index].style.backgroundColor = blockColor;
-        // setOrderColor(blockColor)
         break;
       default:
       // code block
@@ -83,43 +70,43 @@ const ListOfOrders = () => {
     // function updateOrderStatus(index, event, status) {
     const updateOrderStatus = async (index, event, status, orderNumber) => {
 
-      console.log("Update Order Status!")
-      console.log(event)
-      console.log(" ==== Target ==== ")
-      console.log(event.target.style.backgroundColor)
-      console.log(event)
-      console.log(index)
-      console.log(status)
-      console.log(orderNumber)
+      // console.log("Update Order Status!")
+      // console.log(event)
+      // console.log(" ==== Target ==== ")
+      // console.log(event.target.style.backgroundColor)
+      // console.log(event)
+      // console.log(index)
+      // console.log(status)
+      // console.log(orderNumber)
 
       let blockColor = "#bec1be"
 
-      var c = document.getElementById("listOfOrdersID").childNodes;
+      var orderListEL = document.getElementById("listOfOrdersID").childNodes;
 
       switch (status) {
         case "Submitted":
           // code block
           blockColor = "#31a531"
-          console.log("Submitted = " + status + " | Color = " + blockColor)
-          c[index].style.backgroundColor = blockColor;
+          // console.log("Submitted = " + status + " | Color = " + blockColor)
+          orderListEL[index].style.backgroundColor = blockColor;
           // setOrderColor(blockColor)
           break;
         case "WIP":
           blockColor = "#fff200"
-          console.log("Submitted = " + status + " | Color = " + blockColor)
-          c[index].style.backgroundColor = blockColor;
+          // console.log("Submitted = " + status + " | Color = " + blockColor)
+          orderListEL[index].style.backgroundColor = blockColor;
           // setOrderColor(blockColor)
           break;
         case "Ready":
           blockColor = "#00ff00"
-          console.log("Submitted = " + status + " | Color = " + blockColor)
-          c[index].style.backgroundColor = blockColor;
+          // console.log("Submitted = " + status + " | Color = " + blockColor)
+          orderListEL[index].style.backgroundColor = blockColor;
           // setOrderColor(blockColor)
           break;
         case "Picked Up":
           blockColor = "#a1a1a1"
-          console.log("Submitted = " + status + " | Color = " + blockColor)
-          c[index].style.backgroundColor = blockColor;
+          // console.log("Submitted = " + status + " | Color = " + blockColor)
+          orderListEL[index].style.backgroundColor = blockColor;
           // setOrderColor(blockColor)
           break;
         default:
@@ -134,14 +121,8 @@ const ListOfOrders = () => {
 
     }
 
-    // console.log("finalItemsArray")
-    // console.log(finalItemsArray)
-
     orderList.push(
       <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: initalColor }}>
-        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: orderColor }}> */}
-        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4" style={{ backgroundColor: this.target.style.backgroundColor }}> */}
-        {/* // <li key={item} className="orderRow p-3 mx-2 mb-4"> */}
         <div className="orderStatusBox p-2">
           <h1 className="mt-0">Order Number: {orderArray[0]}</h1>
         </div>
@@ -156,20 +137,6 @@ const ListOfOrders = () => {
             {finalItemsArray}
           </div>
         </h1>
-        {/* <h1 className="mt-0">QTY: {orderArray[2]}</h1> */}
-        {/* <div className="updateStatusBox m-2 p-2">
-          <div className="dropdown mt-3">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown button
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <p className="dropdown-item" >Submitted</p>
-              <p className="dropdown-item" >WIP</p>
-              <p className="dropdown-item" >Ready</p>
-              <p className="dropdown-item" >Complete</p>
-            </div>
-          </div>
-        </div> */}
         <div className="d-flex statusBox mt-4 px-2 pb-3">
           <h1 className="mt-0 col bordertext-center mt-4 statusText">Status: {orderArray[3]}</h1>
           <div className="dropdown mt-3 col text-center">
@@ -187,15 +154,6 @@ const ListOfOrders = () => {
 
       </li >)
 
-    // console.log(" ====== Order Array ======")
-    // console.log(orderArray)
-    // console.log(" ====== ITEM Array ======")
-    // console.log(itemArray)
-    // console.log(" ====== QTY Array ======")
-    // console.log(qtyArray)
-
-    // console.log(item)
-    // console.log(index)
   }
 
 
@@ -209,8 +167,8 @@ const ListOfOrders = () => {
     // console.log("GET ALL ORDERS!")
     // console.log(data.getAllOrders)
 
-    console.log("GEt All Orders!")
-    console.log(data.getAllOrders[1])
+    // console.log("GEt All Orders!")
+    // console.log(data.getAllOrders[1])
 
     data.getAllOrders.forEach(newOrderRow);
 
